e08cfd5477c28f606bdd8c3236e463b043d9cc9f
sujoncoder <sujonsheikh.in@gmail.com>
2024-12-13 21:04:12 +0600

    done structure

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/hello", (req, res) => res.send("hello"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
