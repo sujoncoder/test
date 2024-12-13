fee7f12ee3b10655067066f5d7eb59de77667dd2
sujoncoder <sujonsheikh.in@gmail.com>
2024-12-13 21:11:25 +0600

    node added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
-app.get("/hello", (req, res) => res.send("hello"));
+app.get("/hello", (req, res) => res.send("hello world broooo..."));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
